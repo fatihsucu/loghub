112eadd761bde7dd53a0a0627f03bbbfdb437571
fatihsucu <fatihsucu0@gmail.com>
2014-08-20 17:39:16 +0300

    deletes hardcodes in tester

--- tests/tester.py
+++ tests/tester.py
@@ -8,9 +8,11 @@ import run
 
 
 class LoghubTestCase(unittest.TestCase):
+
     """docstring for LoghubTestCase"""
+    
     def setUp(self):
-        db = pymongo.MongoClient()['loghub_test']
+        self.db = pymongo.MongoClient()['loghub_dev']
         run.app.config['TESTING'] = True
         self.app = run.app.test_client()
 
@@ -20,10 +22,10 @@ class LoghubTestCase(unittest.TestCase):
         db.drop_database('loghub_test')
 
 
-    def test_create_user(self):
+    def test_a_create_user(self):        
         rv  = self.app.post("/API/v1/users",data=dict(email="mysexyemail@mail.com",
                                                 password="mysexypassword"
-                                                ))
+                                                ))        
         assert "Success" in rv.data
         rv  = self.app.post("/API/v1/users",data=dict(email="mysexyemailmail.com",
                                                     password="mysexypassword"
@@ -34,20 +36,20 @@ class LoghubTestCase(unittest.TestCase):
                                                     ))
         assert "Password is invalid" in rv.data
 
-    def test_get_user(self):
-        rv = self.app.get("/API/v1/user/email=mysexyemail@mail.com&password=mysexypassword")
+
+    def test_b_get_user(self):        
+        rv = self.app.get("/API/v1/user/email=mysexyemail@mail.com&password=mysexypassword")        
         assert "Success" in rv.data
         rv = self.app.get("/API/v1/user/email=mysexyemailmail.com&password=mysexypassword")
         assert "Email is invalid" in rv.data
         rv = self.app.get("/API/v1/user/email=mysexyemail@mail.com&password=mord")
         assert "Incorrect email or password." in rv.data
 
-
-    def test_reset_credential(self):
+    def test_c_reset_credential(self):        
         rv = self.app.post("/API/v1/user/credential", data=dict(
                                                     email="mysexyemail@mail.com",
                                                     password="mysexypassword"
-                                                    ))
+                                                    ))        
         assert "Success" in rv.data
         rv = self.app.post("/API/v1/user/credential", data=dict(
                                                     email="mysexyemailmail.com",
@@ -60,13 +62,15 @@ class LoghubTestCase(unittest.TestCase):
                                                     ))
         assert "Password is invalid" in rv.data
 
-    def test_change_user_mail(self):
-        h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
+    def test_d_change_user_mail(self):        
+        h = Headers()        
+        credential = self.db.users.find_one({"email":"mysexyemail@mail.com"})["credential_id"] 
+        
+        h.add("Authorization","credential_id {}".format(credential))
         rv = self.app.put('/API/v1/user/email',data=dict(new_email="mynewemail@mail.com",
                                                         password="mysexypassword"),
                                                     headers=h
-                                                    )
+                                                    )        
         assert "Success" in rv.data
         rv = self.app.put('/API/v1/user/email',data=dict(new_email="mynewemail@mail.com",
                                                         password="myseassword"),
@@ -79,25 +83,29 @@ class LoghubTestCase(unittest.TestCase):
                                                     )
         assert "Email is invalid." in rv.data
 
-    def test_remember_account(self):
+
+    def test_e_remember_account(self):
+        
         rv = self.app.post("/API/v1/auth/remember", data=dict(
-                                                    email="mysexyemail@mail.com"                                                    
-                                                    ))
+                                                    email="mynewemail@mail.com"                                                    
+                                                    ))        
         assert "Success" in rv.data
         rv = self.app.post("/API/v1/auth/remember", data=dict(
-                                                    email="mysexyemailmail.com"                                                    
+                                                    email="mynewemail.com"                                                    
                                                     ))
         assert "Email is invalid." in rv.data
 
-    def test_reseuser_password(self):
+
+    def test_f_reset_user_password(self):        
+        code = self.db.codes.find_one({"email":"mynewemail@mail.com"})["code"]
         rv = self.app.post("/API/v1/auth/reset_password", data=dict(
-                                                    email="mysexyemail@mail.com",
+                                                    email="mynewemail@mail.com",
                                                     new_password="123456",
-                                                    code="e5b63da356d41bd7ab1fbdf87d5606f9"                                                 
-                                                    ))
+                                                    code="{}".format(code)                                                 
+                                                    ))        
         assert "Success" in rv.data
         rv = self.app.post("/API/v1/auth/reset_password", data=dict(
-                                                    email="mysexyemail@mail.com",
+                                                    email="mynewemail@mail.com",
                                                     new_password="123456",
                                                     code="sssss"                                                 
                                                     ))
@@ -105,122 +113,141 @@ class LoghubTestCase(unittest.TestCase):
         rv = self.app.post("/API/v1/auth/reset_password", data=dict(
                                                     email="mysexyemailmail.com",
                                                     new_password="123456",
-                                                    code="e5b63da356d41bd7ab1fbdf87d5606f9"                                                 
+                                                    code="{}".format(code)                                                 
                                                     ))
         assert "Email is invalid." in rv.data
 
-    def test_ragister_app(self):
+
+    def test_g_register_app(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
         rv = self.app.post("/API/v1/applications", data=dict(
                                                     name="mysexyapp",
                                                     ),
-                                                    headers=h)
+                                                    headers=h)        
         assert "Success" in rv.data        
         rv = self.app.post("/API/v1/applications", data=dict(
                                                     name="mysexyapp",
                                                     ))
         assert "Credential id required" in rv.data
         
-    def test_get_apps(self):
+
+    def test_h_get_apps(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
-        rv = self.app.get("/API/v1/applications", headers=h)
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
+        rv = self.app.get("/API/v1/applications", headers=h)        
         assert "Success" in rv.data
         rv = self.app.get("/API/v1/applications")
         assert "Credential id required" in rv.data
 
-    def test_get_app(self):
+    def test_j_get_app(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
-        rv = self.app.get("/API/v1/applications/a34665aa7f67e76dccba749cb7931a21/", headers=h)
+        APP_TOKEN = self.db.apps.find_one({"name":"mysexyapp"})["APP_TOKEN"]        
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
+        rv = self.app.get("/API/v1/applications/{}/".format(APP_TOKEN), headers=h)        
         assert "Success" in rv.data
-        rv = self.app.get("/API/v1/applications/a34665aa7f67e76dccba749cb7931a21/")
+        rv = self.app.get("/API/v1/applications/{}/".format(APP_TOKEN))
         assert "Credential id required" in rv.data
-        rv = self.app.get("/API/v1/applications/278b7ea98443a159c157fc451428/", headers=h)
+        rv = self.app.get("/API/v1/applications/{}1/".format(APP_TOKEN), headers=h)
         assert "Invalid APP_TOKEN" in rv.data
 
-    def test_reset_app_token(self):
+    def test_v_reset_app_token(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
-        rv = self.app.put("/API/v1/applications/a34665aa7f67e76dccba749cb7931a21/token", headers=h)
+        APP_TOKEN = self.db.apps.find_one({"name":"mysexyapp"})["APP_TOKEN"]
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
+        rv = self.app.put("/API/v1/applications/{}/token".format(APP_TOKEN), headers=h)        
         assert "Success" in rv.data
-        rv = self.app.put("/API/v1/applications/a34665aa7f67e76dccba749cb7931a21/token", headers=h)
+        rv = self.app.put("/API/v1/applications/{}/token".format(APP_TOKEN), headers=h)
         assert "Couldn't find any registered apps" in rv.data
 
-    def test_delete_apps(self):
+    def test_z_delete_apps(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
-        rv = self.app.delete("/API/v1/applications/e3db37ee279f6340133e9b8d0b313da0/", headers=h)
+        APP_TOKEN = self.db.apps.find_one({"name":"mysexyapp"})["APP_TOKEN"]
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
+        rv = self.app.delete("/API/v1/applications/{}/".format(APP_TOKEN), headers=h)        
         assert "Success" in rv.data
-        rv = self.app.delete("/API/v1/applications/e3db37ee279f6340133e9b8d0b313da0/")
+        rv = self.app.delete("/API/v1/applications/{}/".format(APP_TOKEN))
         assert "Credential id required" in rv.data
-    
 
-
-    def test_logging(self):
+    def test_m_logging(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
-        rv = self.app.post("/API/v1/applications/360884125b4f43d9f271af997a88bc44/",data=dict(
+        APP_TOKEN = self.db.apps.find_one({"name":"mysexyapp"})["APP_TOKEN"]
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
+        rv = self.app.post("/API/v1/applications/{}/".format(APP_TOKEN),data=dict(
                                                                             message="something bad happened"                                                                              
                                                                               ),
                                                                             headers=h
-                                                                            )
+                                                                            )        
         assert "Success" in rv.data
-        rv = self.app.post("/API/v1/applications/360884125b4f43d9f271af997a88bc44/",data=dict(
+        rv = self.app.post("/API/v1/applications/{}/".format(APP_TOKEN),data=dict(
                                                                             message="something bad happened"                                                                              
                                                                               )                                                                            
                                                                             )
         assert "Credential id required" in rv.data
 
-    def test_query_log(self):
+
+    def test_n_query_log(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
-        rv = self.app.get("/API/v1/logs",headers=h)
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
+        rv = self.app.get("/API/v1/logs",headers=h)        
         assert "Success" in rv.data
         rv = self.app.get("/API/v1/logs")
         assert "Credential id required" in rv.data
 
-    def test_register_alarm(self):
+    def test_o_register_alarm(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
         rv = self.app.post("/API/v1/alarms", data=dict(alarm="some_alarm"))
         assert "CREDENTIAL_ID required." in rv.data
-        rv = self.app.post("/API/v1/alarms", data=dict(alarm="some_alarm",receivers="asdad@gmail.com",name="myAlarm"),headers=h)
+        rv = self.app.post("/API/v1/alarms", data=dict(alarm="some_alarm",receivers="asdad@gmail.com",name="myAlarm"),headers=h)        
         assert "Success" in rv.data
         rv = self.app.post("/API/v1/alarms", data=dict(alarm="some_alarm",receivers="asdad@gmail.com"),headers=h)
         assert "Name and Receivers must be specified." in rv.data
         rv = self.app.post("/API/v1/alarms", data=dict(alarm="some_alarm",name="asdad"),headers=h)
         assert "Name and Receivers must be specified." in rv.data
 
-    def test_get_alarms(self):
+    def test_p_get_alarms(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
-        rv = self.app.get("/API/v1/alarms", headers=h)
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
+        rv = self.app.get("/API/v1/alarms", headers=h)        
         assert "Success" in rv.data
         rv = self.app.get("/API/v1/alarms")
         assert "CREDENTIAL_ID required." in rv.data
 
-    def test_get_alarm_by_id(self):
+    def test_r_get_alarm_by_id(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
-        rv = self.app.get("/API/v1/alarms/53f3f41c56c02c19f8b3d036", headers=h)
+        alarm_id = str(self.db.alarms.find_one({"name":"myAlarm"})["_id"])
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
+        rv = self.app.get("/API/v1/alarms/{}".format(alarm_id), headers=h)        
         assert "Success" in rv.data
-        rv = self.app.get("/API/v1/alarms/53f3f36c56c02c19fbb3d024")
+        rv = self.app.get("/API/v1/alarms/{}".format(alarm_id))
         assert "CREDENTIAL_ID required." in rv.data
-        rv = self.app.get("/API/v1/alarms/53f3f36c11102c19fbb3d024", headers=h)
+        rv = self.app.get("/API/v1/alarms/{}b".format(alarm_id[:-1]), headers=h)
         assert "You do not have an alarm with that ID." in rv.data
 
-    def test_delete_alarm(self):
+    def test_s_delete_alarm(self):        
         h = Headers()
-        h.add("Authorization","credential_id 8d2a045b159b29808ceb455714988450")
-        rv = self.app.delete("/API/v1/alarms/53f3f36c56c02c19fbb3d024", headers=h)
+        alarm_id = str(self.db.alarms.find_one({"name":"myAlarm"})["_id"])
+        credential = self.db.users.find_one({"email":"mynewemail@mail.com"})["credential_id"]        
+        h.add("Authorization","credential_id {}".format(credential))
+        rv = self.app.delete("/API/v1/alarms/{}".format(alarm_id), headers=h)        
         assert "Success" in rv.data
-        rv = self.app.delete("/API/v1/alarms/53f3f36c56c02c19fbb3d024")
+        rv = self.app.delete("/API/v1/alarms/{}".format(alarm_id))
         assert "CREDENTIAL_ID required." in rv.data
-        rv = self.app.delete("/API/v1/alarms/53f3f36c11102c19fbb3d024", headers=h)        
+        rv = self.app.delete("/API/v1/alarms/{}".format(alarm_id), headers=h)        
         assert "You do not have an alarm with that ID." in rv.data
+
 
 if __name__ == '__main__':
     unittest.main()
-   
+    
